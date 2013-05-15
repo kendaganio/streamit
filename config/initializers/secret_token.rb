81c0a88043482e15651245074c3a51614a62a12a
Ken-Lauren Daganio <ken.daganio@indinero.com>
2013-05-15 13:28:10 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Streamit::Application.config.secret_token = 'f84dea1afe7c33cb85252220af052c33133716e86a68fdcd64b96a23189fe529810730a3e066e4a73d37124fea79aa7f8225bd5be52a6ddd767d220cdf720cc9'
