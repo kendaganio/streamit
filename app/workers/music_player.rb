6164e1ed4737665f408eacf2fd0924017e643651
Ken-Lauren Daganio <ken.daganio@indinero.com>
2013-05-16 13:12:00 +0800

    Adds log message for music player worker

--- app/workers/music_player.rb
+++ app/workers/music_player.rb
@@ -4,6 +4,7 @@ class MusicPlayer
 
   def self.perform(track_id)
     @track = Track.find(track_id)
+    puts "Playing track: #{@track.name}"
     exec 'mpg123', '-q', @track.track.path
     puts 'this is a test'
   end
